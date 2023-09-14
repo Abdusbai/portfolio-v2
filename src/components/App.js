6994fa66c64725fee78ca6842fe013672357eabb
Abdessamad Sbai <sbai.itsd@gmail.com>
2023-09-14 19:57:41 +0100

    adding contact us section

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import MainHeader from "./header/MainHeader";
 import MainAbout from "./about/MainAbout";
 import MainSkills from "./skills/MainSkills";
 import MainProject from "./projects/MainProject";
+import MainContact from "./contact/MainContact";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <MainAbout />
       <MainSkills />
       <MainProject />
+      <MainContact />
     </>
   );
 }
--- src/components/header/SocialMediaIcons.js
+++ src/components/header/SocialMediaIcons.js
@@ -31,7 +31,11 @@ function SocialMediaIcons() {
         </svg>
       </a>
 
-      <a href="https://twitter.com/abdu_sbai" target="_blank" rel="noreferrer">
+      <a
+        href="https://twitter.com/ASbaiOfficial"
+        target="_blank"
+        rel="noreferrer"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="social-icon tw"
@@ -48,7 +52,7 @@ function SocialMediaIcons() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="social-icon ln"
+          className="social-icon lin"
           viewBox="0 0 24 24"
         >
           <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" />
