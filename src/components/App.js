e75a2cabd8ce6bdfd6269a0603e77d68479de39c
Abdessamad Sbai <sbai.itsd@gmail.com>
2023-09-15 11:28:28 +0100

    finale touches

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import MainHeader from "./header/MainHeader";
 import MainAbout from "./about/MainAbout";
 import MainSkills from "./skills/MainSkills";
@@ -7,17 +7,36 @@ import MainContact from "./contact/MainContact";
 import MainFooter from "./footer/MainFooter";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "../LanguageSwitcher";
+import Loader from "./Loader";
 
 export default function App() {
   const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+      document.body.classList.remove("hide-scroll");
+    }, 1500);
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) {
+      document.body.classList.add("hide-scroll");
+    }
+  }, [isLoading]);
+
   return (
     <>
-      <MainHeader t={t} LanguageSwitcher={LanguageSwitcher} />
-      <MainAbout t={t} />
-      <MainSkills t={t} />
-      <MainProject t={t} />
-      <MainContact t={t} />
-      <MainFooter t={t} />
+      {isLoading ? <Loader /> : ""}
+      <>
+        <MainHeader t={t} LanguageSwitcher={LanguageSwitcher} />
+        <MainAbout t={t} />
+        <MainSkills t={t} />
+        <MainProject t={t} />
+        <MainContact t={t} />
+        <MainFooter t={t} />
+      </>
     </>
   );
 }
